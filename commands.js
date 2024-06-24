6d7841f30efd5f73a01bdd53d79c143442a3eb56
Xx_Zer0_xX <127456016+Xxzer042xX@users.noreply.github.com>
2024-06-24 02:38:49 +0200

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,16 +1,16 @@
 // Création d'un ensemble pour suivre les membres salués aujourd'hui
 const greetedMembersToday = new Set();
 
-// Fonction pour gérer la commande !piscine
+// Fonction pour gérer la commande 'votre_commande'
 async function handlePiscineCommand(message) {
     try {
         // Ajoute un rôle spécifique à l'utilisateur
-        await message.member.roles.add('votre id de role');
+        await message.member.roles.add('ID_DU_ROLE'); // Remplacez 'ID_DU_ROLE' par votre ID de rôle
         // Répond à l'utilisateur avec un message de confirmation
-        await message.reply('votre message');
+        await message.reply('Votre message de confirmation');
     } catch (error) {
         // Logge une erreur en cas de problème
-        console.error("Erreur lors de l'exécution de la commande!", error);
+        console.error("Erreur lors de l'exécution de la commande 'votre commande :", error);//personaliser votre sortie
     }
 }
 
@@ -21,7 +21,7 @@ async function handleDailyGreetings(message) {
         // Ajoute l'utilisateur à l'ensemble des membres salués
         greetedMembersToday.add(message.author.id);
         // Envoie un message de salutation dans un canal spécifique
-        await sendMessage(message.client, 'votre ide de canal', `Votre message${message.author}! votre message`);
+        await sendMessage(message.client, 'ID_DU_CANAL', `Votre message de salutation ${message.author}!`); // Remplacez 'ID_DU_CANAL' par votre ID de canal
     }
 }
 
