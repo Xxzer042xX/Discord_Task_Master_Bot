d45d90b885068f34ae19240fa4296e2889f27d25
Xxzer042xX <eljok87@gmail.com>
2024-06-26 17:33:23 +0200

    update and add greetedMember.js correctif

---
+++ greetedMembers.js
@@ -0,0 +1,7 @@
+// greetedMembers.js
+
+// Création d'un ensemble pour suivre les membres salués aujourd'hui
+const greetedMembersToday = new Set();
+
+// Exportation de l'ensemble pour l'utiliser dans d'autres fichiers
+module.exports = { greetedMembersToday };
